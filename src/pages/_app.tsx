bb9d324abd3e1b13e2a77088434abe5c73b53096
dffuqp <yuriivasilchuk.work@gmail.com>
2024-02-25 23:45:35 +0200

    feat(*): initialize project

---
+++ src/pages/_app.tsx
@@ -0,0 +1,21 @@
+import { FC } from 'react';
+
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+
+const App: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Head>
+        <title>New nextJs app</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+};
+
+export default App;
